9a4566ce44977a18eda2e008ad2024de28fa6533
Shankhadweep Hazra <119057592+Shankhadweep@users.noreply.github.com>
2023-06-28 10:21:31 +0530

    Created Johnson's Algorithm in Java

--- Java/Graphs/Johnson's Algorithm.java
+++ Java/Graphs/Johnson's Algorithm.java
@@ -1,204 +1,180 @@
+import java.util.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 class Edge {
-    int source;
-    int destination;
-    int weight;
+    int source, destination, weight;
 
-    public Edge(int source, int destination, int weight) {
+    Edge(int source, int destination, int weight) {
         this.source = source;
         this.destination = destination;
         this.weight = weight;
     }
 }
 
-class BellmanFordResult {
-    int[] distance;
-    int[] predecessor;
+class Graph {
+    int V, E;
+    List<Edge> edges;
 
-    public BellmanFordResult(int numVertices) {
-        distance = new int[numVertices];
-        predecessor = new int[numVertices];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-        Arrays.fill(predecessor, -1);
-    }
-}
-
-class DijkstraResult {
-    int[] distance;
-
-    public DijkstraResult(int numVertices) {
-        distance = new int[numVertices];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-    }
-}
-
-public class JohnsonsAlgorithm {
-    private static final int INF = Integer.MAX_VALUE;
-    private int numVertices;
-    private List<Edge> edges;
-
-    public JohnsonsAlgorithm(int numVertices) {
-        this.numVertices = numVertices;
+    Graph(int V, int E) {
+        this.V = V;
+        this.E = E;
         edges = new ArrayList<>();
     }
 
-    public void addEdge(int source, int destination, int weight) {
+    void addEdge(int source, int destination, int weight) {
         Edge edge = new Edge(source, destination, weight);
         edges.add(edge);
     }
+}
 
-    private void initializeGraph(int[][] graph) {
-        for (int i = 0; i < numVertices; i++) {
-            for (int j = 0; j < numVertices; j++) {
-                if (i == j) {
-                    graph[i][j] = 0;
-                } else {
-                    graph[i][j] = INF;
-                }
-            }
-        }
-    }
+class JohnsonAlgorithm {
+    static final int INF = Integer.MAX_VALUE;
 
-    private void bellmanFord(int[][] graph, int[] distance, int[] predecessor, int source) {
-        distance[source] = 0;
+    void Johnson(Graph graph) {
+        int V = graph.V;
+        int E = graph.E;
 
-        for (int i = 1; i <= numVertices - 1; i++) {
-            for (Edge edge : edges) {
-                int u = edge.source;
-                int v = edge.destination;
-                int weight = edge.weight;
+        int[] distance = new int[V + 1];
+        Arrays.fill(distance, INF);
+        distance[V] = 0;
 
-                if (distance[u] != INF && distance[u] + weight < distance[v]) {
-                    distance[v] = distance[u] + weight;
-                    predecessor[v] = u;
-                }
-            }
-        }
+        // Add a new edge with weight 0 from the extra vertex to all other vertices
+        graph.addEdge(V, 0, 0);
 
-        for (Edge edge : edges) {
-            int u = edge.source;
-            int v = edge.destination;
-            int weight = edge.weight;
-
-            if (distance[u] != INF && distance[u] + weight < distance[v]) {
-                distance[v] = -INF;
-                predecessor[v] = -1;
-            }
-        }
-    }
-
-    private void dijkstra(int[][] graph, int[] distance, int source) {
-        boolean[] visited = new boolean[numVertices];
-        distance[source] = 0;
-
-        for (int i = 0; i < numVertices - 1; i++) {
-            int minDistance = INF;
-            int minIndex = -1;
-
-            for (int j = 0; j < numVertices; j++) {
-                if (!visited[j] && distance[j] <= minDistance) {
-                    minDistance = distance[j];
-                    minIndex = j;
-                }
-            }
-
-            visited[minIndex] = true;
-
-            for (Edge edge : edges) {
-                int u = edge.source;
-                int v = edge.destination;
-                int weight = edge.weight;
-
-                if (!visited[v] && graph[u][v] != INF && distance[u] != INF && distance[u] + weight < distance[v]) {
-                    distance[v] = distance[u] + weight;
-                }
-            }
-        }
-    }
-
-    public void johnsonsAlgorithm() {
-        int[][] graph = new int[numVertices][numVertices];
-
-        initializeGraph(graph);
-
-        int[][] augmentedGraph = new int[numVertices + 1][numVertices + 1];
-
-        for (int i = 0; i < numVertices; i++) {
-            for (int j = 0; j < numVertices; j++) {
-                augmentedGraph[i][j] = graph[i][j];
-            }
-        }
-
-        for (int i = 0; i < numVertices; i++) {
-            augmentedGraph[numVertices][i] = 0;
-        }
-
-        int[] bellmanFordDistance = new int[numVertices + 1];
-        int[] bellmanFordPredecessor = new int[numVertices + 1];
-
-        Arrays.fill(bellmanFordDistance, INF);
-        Arrays.fill(bellmanFordPredecessor, -1);
-
-        bellmanFord(augmentedGraph, bellmanFordDistance, bellmanFordPredecessor, numVertices);
-
-        if (bellmanFordDistance[numVertices] == -INF) {
-            System.out.println("Graph contains negative weight cycle. Johnson's algorithm cannot be applied.");
+        // Run Bellman-Ford algorithm to get the shortest distances from the extra vertex
+        if (!BellmanFord(graph, V, distance)) {
+            System.out.println("Graph contains negative-weight cycles.");
             return;
         }
 
-        int[][] newGraph = new int[numVertices][numVertices];
+        // Remove the extra edge
+        graph.edges.remove(graph.E);
 
-        for (int i = 0; i < numVertices; i++) {
-            for (int j = 0; j < numVertices; j++) {
-                newGraph[i][j] = augmentedGraph[i][j];
-            }
-        }
-
-        for (int u = 0; u < numVertices; u++) {
-            for (int v = 0; v < numVertices; v++) {
-                if (newGraph[u][v] != INF) {
-                    newGraph[u][v] = newGraph[u][v] + bellmanFordDistance[u] - bellmanFordDistance[v];
+        // Re-weight the edges
+        int[] h = new int[V];
+        for (int i = 0; i < V; i++) {
+            for (Edge edge : graph.edges) {
+                int u = edge.source;
+                int v = edge.destination;
+                int weight = edge.weight;
+                if (distance[u] != INF && distance[u] + weight < distance[v]) {
+                    distance[v] = distance[u] + weight;
                 }
             }
         }
 
-        for (int u = 0; u < numVertices; u++) {
-            int[] dijkstraDistance = new int[numVertices];
-            Arrays.fill(dijkstraDistance, INF);
-            dijkstraDistance[u] = 0;
+        for (Edge edge : graph.edges) {
+            int u = edge.source;
+            int v = edge.destination;
+            int weight = edge.weight;
+            edge.weight = weight + distance[u] - distance[v];
+        }
 
-            dijkstra(newGraph, dijkstraDistance, u);
-
-            System.out.println("Shortest paths from vertex " + u + ":");
-
-            for (int v = 0; v < numVertices; v++) {
-                if (dijkstraDistance[v] == INF) {
-                    System.out.println("Vertex " + v + " is unreachable");
+        // Run Dijkstra's algorithm for each vertex
+        for (int i = 0; i < V; i++) {
+            int[] result = Dijkstra(graph, i);
+            System.out.println("Shortest distances from vertex " + i + ": ");
+            for (int j = 0; j < V; j++) {
+                if (result[j] == INF) {
+                    System.out.print("INF ");
                 } else {
-                    System.out.println("Vertex " + v + ": Distance = " + dijkstraDistance[v] + ", Predecessor = " + u);
+                    System.out.print(result[j] + " ");
                 }
             }
-
             System.out.println();
         }
     }
 
+    boolean BellmanFord(Graph graph, int source, int[] distance) {
+        int V = graph.V;
+        int E = graph.E;
+
+        Arrays.fill(distance, INF);
+        distance[source] = 0;
+
+        for (int i = 1; i < V; i++) {
+            for (Edge edge : graph.edges) {
+                int u = edge.source;
+                int v = edge.destination;
+                int weight = edge.weight;
+                if (distance[u] != INF && distance[u] + weight < distance[v]) {
+                    distance[v] = distance[u] + weight;
+                }
+            }
+        }
+
+        for (Edge edge : graph.edges) {
+            int u = edge.source;
+            int v = edge.destination;
+            int weight = edge.weight;
+            if (distance[u] != INF && distance[u] + weight < distance[v]) {
+                return false; // Negative-weight cycle found
+            }
+        }
+
+        return true;
+    }
+
+    int[] Dijkstra(Graph graph, int source) {
+        int V = graph.V;
+        int[] distance = new int[V];
+        boolean[] visited = new boolean[V];
+
+        Arrays.fill(distance, INF);
+        distance[source] = 0;
+
+        for (int count = 0; count < V - 1; count++) {
+            int minDist = INF;
+            int minDistIndex = -1;
+
+            // Find the vertex with the minimum distance
+            for (int v = 0; v < V; v++) {
+                if (!visited[v] && distance[v] <= minDist) {
+                    minDist = distance[v];
+                    minDistIndex = v;
+                }
+            }
+
+            int u = minDistIndex;
+            visited[u] = true;
+
+            // Update distance value of the adjacent vertices
+            for (Edge edge : graph.edges) {
+                int weight = edge.weight;
+                if (!visited[edge.destination] && weight != INF && distance[u] != INF && distance[u] + weight < distance[edge.destination]) {
+                    distance[edge.destination] = distance[u] + weight;
+                }
+            }
+        }
+
+        return distance;
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
-        int numVertices = 5;
-        JohnsonsAlgorithm johnsonsAlgorithm = new JohnsonsAlgorithm(numVertices);
+        Scanner in=new Scanner(System.in);
+        int s=0,d=0,w=0;
+        int V = 0;
+        int E = 0;
+        System.out.print("Enter the number of vertices:");
+        V=in.nextInt();
+        System.out.print("Enter the number of edges:");
+        E=in.nextInt();
 
-        johnsonsAlgorithm.addEdge(0, 1, -1);
-        johnsonsAlgorithm.addEdge(0, 2, 4);
-        johnsonsAlgorithm.addEdge(1, 2, 3);
-        johnsonsAlgorithm.addEdge(1, 3, 2);
-        johnsonsAlgorithm.addEdge(1, 4, 2);
-        johnsonsAlgorithm.addEdge(3, 2, 5);
-        johnsonsAlgorithm.addEdge(3, 1, 1);
-        johnsonsAlgorithm.addEdge(4, 3, -3);
+        Graph graph = new Graph(V, E);
+        for(int i=1;i<=E;i++)
+            {
+                System.out.print("Enter the source , destination , weight:");
+                s=in.nextInt();
+                d=in.nextInt();
+                w=in.nextInt();
+                graph.addEdge(s, d, w);
+            }
 
-        johnsonsAlgorithm.johnsonsAlgorithm();
+        JohnsonAlgorithm johnson = new JohnsonAlgorithm();
+        johnson.Johnson(graph);
     }
 }
